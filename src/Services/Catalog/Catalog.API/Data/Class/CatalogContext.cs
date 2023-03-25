c8f40fb659ac6ae746440050457ee5eff4b2b3e9
Lavanya Rajendran <slavan718@gmail.com>
2023-03-25 22:39:32 +0530

    repo

---
+++ src/Services/Catalog/Catalog.API/Data/Class/CatalogContext.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data.Class
+{
+    public class CatalogContext : ICatalogContext
+    {
+        private readonly IConfiguration _configuration;
+      public  IMongoCollection<Product> Products { get => throw new NotImplementedException(); }
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+            var _client = new MongoClient(_configuration["DatabaseSettings:ConnectionString"]);
+            var _database = _client.GetDatabase(_configuration["DatabaseSettings:DatabaseName"]);
+           var Products = _database.GetCollection<Product>(_configuration["DatabaseSettings:CollectionName"]);
+            CatalogContextSeed.SeedData(Products);
+        }
+
+    }
+}
